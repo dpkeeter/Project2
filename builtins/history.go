989c15d10fad89eec5b7b3581a0a31d7eadd0445
David Keeter <davidpkeeter@gmail.com>
2023-05-10 14:23:40 -0500

    created base for History command

--- builtins/history.go
+++ builtins/history.go
@@ -26,33 +26,5 @@ func History(currenthistory []string, name string, args ...string) error {
 		fmt.Println("Print History in reverse Here")
 	}
 
-	/*	toRemove := make([]string, 0)
-		for i := 0; i < len(args); i++ {
-			if args[i] == "-u" {
-				if len(args) < i+2 {
-					return fmt.Errorf("%w: -u requires an argument", ErrInvalidArgCount)
-				}
-				toRemove = append(toRemove, args[i+1])
-				i++
-			}
-		}
-
-		toShow := make([]string, 0)
-		for _, env := range os.Environ() {
-			show := true
-			for _, v := range toRemove {
-				if strings.HasPrefix(env, v+"=") {
-					show = false
-					break
-				}
-			}
-			if show {
-				toShow = append(toShow, env)
-			}
-		}
-
-		_, err := fmt.Fprintln(w, strings.Join(toShow, "\n"))
-
-		return err*/
 	return nil
 }
